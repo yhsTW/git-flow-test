aafb5866f82bc5ed6cde0b1cd669befc3a7bd1c3
YHS-mac <yhs0823@twinny.co.kr>
2022-03-18 15:04:45 +0900

    conflict test

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ console.log('linked issues')
 console.log('tttttest');
 console.log('test 2')
 console.log('pull request link test')
-<<<<<<< HEAD
-console.log('conflict!!!!!!!!!')
-=======
 console.log('git flow test');
->>>>>>> develop
+console.log('conflict!!!!');
